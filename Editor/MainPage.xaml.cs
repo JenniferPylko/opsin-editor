45655e82b54bfecfc6c18db92b145faffd695360
Jennifer <jen@pylko.cloud>
2023-11-29 10:43:16 -0500

    launches a maui window and a vulkan window gotta make them 1

---
+++ Editor/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Editor;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"{count} dead crewmate";
+		else
+			CounterBtn.Text = $"{count} dead crewmates";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ Editor/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Silk.NET.Core;
+using Silk.NET.Maths;
+using Silk.NET.Vulkan;
+using Silk.NET.Windowing;
+
+namespace Editor;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("Program");
+        logger.LogInformation("THERE IS AN IMPOSTER AMONG US");
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+		var options = WindowOptions.DefaultVulkan with {
+			Size = new Vector2D<int>(800, 600),
+			Title = "Silk.NET Vulkan"
+		};
+		Silk.NET.Windowing.IWindow window = Silk.NET.Windowing.Window.Create(options);
+		window.Initialize();
+		if (window.VkSurface is null) {
+			throw new Exception("Windowing system does not support Vulkan");
+		}
+		return builder.Build();
+	}
+}
